652fb869de14716912766340e0b7dd9ac4527d71
Brett Gilman <bgilman@cheyenne1.ib0.cheyenne.ucar.edu>
2020-01-23 11:35:13 -0700

    Added DAXPY and Inverse files

---
+++ ComputeInverse.f90
@@ -0,0 +1,51 @@
+! Matrix Inverse Function - computes the inverse of the passed in matrix if it exits
+! if the inverse does not exist, then the program will terminate
+
+! Arguments
+! A		REAL (n,n) (input)
+!		User passed in nxn matrix 
+!
+! Ainv	REAL (n,n) (output)
+!		Result of the inverse matrix operation
+!
+! n		INTEGER (input)
+!		Dimension of the matrix
+
+! Sources:
+! DGETRF: http://www.netlib.org/lapack/explore-html/dd/d9a/group__double_g_ecomputational_ga0019443faea08275ca60a734d0593e60.html
+! DGETRI: http://www.netlib.org/lapack/explore-html/dd/d9a/group__double_g_ecomputational_ga56d9c860ce4ce42ded7f914fdb0683ff.html
+
+SUBROUTINE MATRIX_INVERSE(A, Ainv, n)
+     REAL, dimension(n,n), intent(in) :: A ! Input matrix A
+     REAL, dimension(n,n), intent(out) :: Ainv ! Inverse matrix of A
+     INTEGER,  intent(in) :: n ! Dimension of the matrix
+
+     ! Variables for the LAPACK inverse function
+     REAL, dimension(size(A,1)) :: work ! Work Array
+     INTEGER, dimension(size(A,1)) :: ipiv !	pivot indices
+     INTEGER  :: info ! determines if inversion was successful
+
+     ! LAPACK procedures
+     external DGETRF
+     external DGETRI
+
+     Ainv = A ! Save the input matrix
+
+     ! DGETRF computes an LU factorization of a general M-by-N matrix A
+     ! using partial pivoting with row interchanges.
+     call DGETRF(n,n,Ainv,n,ipiv,info)
+
+     ! if info is not zero then the LU factorization was unsuccessful
+     IF (info /= 0) THEN
+          stop 'The input matrix is singular'
+     END IF
+
+     ! DGETRI computes the inverse of a matrix using the LU factorization
+     ! computed by DGETRF.
+     call DGETRI(n, Ainv,n,ipiv,work,n,info)
+
+     IF (info /= 0) THEN
+          stop 'The matrix inversion operation failed'
+     END IF
+     RETURN ! Terminate the function
+END SUBROUTINE MATRIX_INVERSE
---
+++ DAXPY.f90
@@ -0,0 +1,75 @@
+! DAXPY function - adds a scalar multiple of a double precision
+! vector to another double precision vector
+! performs the following operation:
+! y <-- alpha*x + y
+! incx and incy specify the increment between two consecutive elements
+! of respectively vector x and vector y
+
+! ARGUMENTS 
+! n	 	INTEGER (input)
+!		Number of elements in the vectors. If n <= 0, these routines
+!		return without any computation
+!
+! alpha 	DOUBLE PRECISION (input)
+!			
+! x 		DOUBLE PRECISION (input)
+!		Array of dimension (n-1)* |incx| + 1. Contains the vector
+!		to be scaled before summation
+!
+! incx		INTEGER (input)
+!		Increment between elements of x.
+!		If incx = 0, the results will be unpredictable
+!
+! y		DOUBLE PRECISION, (input and output)
+!		array of dimension (n-1)*|incy| + 1
+!		Before calling the routine, y contains the vector to be summed
+!		After the routine ends, y contains the result of the summation
+!
+! incy		INTEGER (input)
+!		Increment between elements of y.
+!		If incy = 0, the results will be unpredictable
+
+! source: http://www.netlib.org/lapack/explore-html/de/da4/group__double__blas__level1_ga8f99d6a644d3396aa32db472e0cfc91c.html#ga8f99d6a644d3396aa32db472e0cfc91c
+
+
+SUBROUTINE DAXPY(n, alpha, x, incx, y, incy)
+
+     ! Arguments
+     INTEGER, intent(in) :: n, incx, incy
+     REAL, intent(in) :: alpha
+     REAL, intent(in) :: x(*)
+     REAL, intent(inout) :: y(*)
+
+     INTEGER m, mp1, ix, iy ! local variables
+
+     IF ((n <= 0) .OR. (alpha == 0.0)) RETURN ! if the size for the vectors passed in are too small or if alpha is 0, then terminate the function
+     IF ((incx == 1) .AND. (incy == 1)) THEN ! code for if the increments are the same 
+          m = mod(n,4) ! take the modulus of n for loop unrolling
+          IF (m /= 0) THEN ! if the loop cannot be unrolled go element by element
+               DO i = 1,m ! perform the DAXPY operation with no loop unrolling
+                    y(i) = y(i) + alpha * x(i)
+               END DO
+          END IF
+
+          IF (n < 4) RETURN
+          mp1 = m + 1
+          DO i = mp1, n, 4 ! perform the operation with loop unrolling (unrolled 4 times)
+               y(i) = y(i) + alpha * x(i)
+               y(i+1) = y(i+1) + alpha * x(i+1)
+               y(i+2) = y(i+2) + alpha * x(i+2)
+               y(i+3) = y(i+3) + alpha * x(i+3)
+          END DO
+
+     ELSE ! code for if the increments are unequal
+          ix = 1 ! indexing variables for if the vectors are not equal in length
+          iy = 1
+          IF (incx < 0) ix = (-n + 1)*incx + 1 ! if the stride variable for vector x is negative, compute the new indexing variable for accessing the end of the vector first
+          IF (incy < 0) iy = (-n + 1)*incy + 1 ! if the stride variable for vector y is negative, compute the new indexing variable for accessing the end of the vector first
+          DO i = 1, n ! compute the DAXPY function and compute the new indices for each vector
+               y(iy) = y(iy) + alpha * x(ix)
+               ix = ix + incx
+               iy = iy + incy
+          END DO
+     END IF
+     RETURN ! terminate the function
+END SUBROUTINE DAXPY
